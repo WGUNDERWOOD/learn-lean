89918149b7a33b30142fd95b6ee4e9257a38756e
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2026-06-08 10:15:39 +0100

    Remove unnecessary hypothesis

--- LearnLean.lean
+++ LearnLean.lean
@@ -3,7 +3,7 @@ import Mathlib
 noncomputable def mean {n : Nat} (x : Fin n -> Real) : Real :=
   (1 : Real) / n * Finset.sum Finset.univ x
 
-theorem variance_nonneg (n : Nat) (hn : 0 < n)
+theorem variance_nonneg (n : Nat)
     (x : Fin n -> Real) :
     mean (fun i => (x i - mean x)^2) >= 0 := by
   apply mul_nonneg
