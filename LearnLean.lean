7cea379e2320b84233027a96b13ee6bd0fd72750
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2026-06-09 21:01:44 +0100

    Give up

--- LearnLean.lean
+++ LearnLean.lean
@@ -19,7 +19,39 @@ theorem variance_nonneg (n : Nat)
 noncomputable def indicator_above (x : Real) (a : Real) : Real :=
   if x >= a then 1 else 0
 
+theorem mean_order (n: Nat) (x: Fin n -> Real) (y: Fin n -> Real) (h1: forall i, x i <= y i):
+  mean x <= mean y := by
+    sorry
+
 theorem markov_ineq (n : Nat) (hn: 0 < n)
   (x : Fin n -> Real) (a: Real) (hx: forall i, x i >= 0) (ha: a > 0) :
     mean (fun i => indicator_above (x i) a) <= mean x / a := by
-  sorry
+      let y := (fun i => indicator_above (x i) a)
+      have h1: (forall i, a * y i <= x i) := by
+        intro i
+        by_cases h2: x i >= a
+        {
+          have h3: y i = 1 := by
+            dsimp [y, indicator_above]
+            simp [h2]
+          have h4: a * y i = a := by
+            rw [h3]
+            simp
+          rw [h4]
+          exact h2
+        }
+        {
+          have h5: y i = 0 := by
+            dsimp [y, indicator_above]
+            simp [h2]
+          rw [h5]
+          simp
+          exact hx i
+        }
+      have h6: (forall i, y i <= x i / a) := by
+        intro i
+        have h7: a * y i <= x i := by
+          exact h1 i
+        exact (le_div_iff₀' ha).mpr (h1 i)
+      sorry
+
