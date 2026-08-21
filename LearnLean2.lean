205b8d60eda899a84d0426fd51b3e8bac176ab20
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2026-08-21 16:47:03 +0100

    Note

--- LearnLean2.lean
+++ LearnLean2.lean
@@ -1,4 +1,4 @@
--- import Mathlib
+import Mathlib
 
 example (a b c : Nat) (h1 : a = b) (h2 : b = c) : a + c = b + b := by
    rw [h1]
@@ -14,3 +14,8 @@ example (a b c : Nat) (h1 : a = b) (h2 : b = c) : a + a = 2 * c := by
   rw [h1]
   rw [← h2]
   rw [Nat.two_mul]
+
+example (x y : Real) : x + y ≤ (abs x) + (abs y) := by
+  --have hx : x ≤ |x| := le_abs_self x
+  --have hy : y ≤ |y| := le_abs_self y
+  --exact add_le_add hx hy
